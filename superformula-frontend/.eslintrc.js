dea0c59704ae7ac9e0a6db73d1c7803b3d81a76c
lamatheus.marques <lamatheus.marques@lamatheusmarquess-MacBook-Pro.local>
2021-11-28 15:15:30 -0300

    Adding and setting prettier

--- superformula-frontend/.eslintrc.js
+++ superformula-frontend/.eslintrc.js
@@ -1,47 +1,56 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-  plugins: ['react', '@typescript-eslint', 'react-hooks'],
-  rules: {
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
-      {
-        allowExpressions: true,
-      },
-    ],
-    'react/prop-types': 'off',
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'function-declaration',
-      },
-    ],
-  },
-};
+        env: {
+                browser: true,
+                es2021: true,
+        },
+        extends: [
+                'plugin:react/recommended',
+                'airbnb',
+                'plugin:@typescript-eslint/recommended',
+                'prettier',
+                'prettier/prettier',
+        ],
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+                ecmaFeatures: {
+                        jsx: true,
+                },
+                ecmaVersion: 12,
+                sourceType: 'module',
+        },
+        settings: {
+                'import/resolver': {
+                        typescript: {},
+                },
+        },
+        plugins: ['react', '@typescript-eslint', 'react-hooks'],
+        rules: {
+                'no-use-before-define': 'off',
+                '@typescript-eslint/no-use-before-define': ['error'],
+                'react/jsx-filename-extension': [
+                        'warn',
+                        { extensions: ['.tsx'] },
+                ],
+                'import/extensions': [
+                        'error',
+                        'ignorePackages',
+                        {
+                                ts: 'never',
+                                tsx: 'never',
+                        },
+                ],
+                '@typescript-eslint/explicit-function-return-type': [
+                        'error',
+                        {
+                                allowExpressions: true,
+                        },
+                ],
+                'react/prop-types': 'off',
+                'react/function-component-definition': [
+                        2,
+                        {
+                                namedComponents: 'function-declaration',
+                        },
+                ],
+        },
+}
