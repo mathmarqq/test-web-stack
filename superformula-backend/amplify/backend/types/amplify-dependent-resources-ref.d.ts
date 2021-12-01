2c5366bf744323f33f3025e94b4835a051a910d3
Matheus Marques <matheusmarques312@gmail.com>
2021-12-01 02:31:10 -0300

    Creating backend with amplify

---
+++ superformula-backend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "superformulabackend": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "getLocationDetails": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
