4187fbe0b59713bd931893a955da3160b36fbeb5
Matheus Marques <matheusmarques312@gmail.com>
2021-12-01 22:00:00 -0300

    Creating get localization Details lambda function

--- superformula-backend/amplify/backend/function/getLocationDetails/src/index.js
+++ superformula-backend/amplify/backend/function/getLocationDetails/src/index.js
@@ -5,16 +5,19 @@
 	MAPBOX_API_URL
 Amplify Params - DO NOT EDIT */
 
+const axios = require('axios')
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+  const { MAPBOX_API_URL, MAPBOX_API_ACCESS_TOKEN } = process.env;
+
+  const { address } = event.arguments;
+  const apiURL = `${MAPBOX_API_URL}/mapbox.places/${address}.json?access_token=${MAPBOX_API_ACCESS_TOKEN}`;
+
+  try {
+    const response = await axios.get(apiURL);
+    const [longitude, latitude] = response.data?.features?.[0].center || [];
+    return { longitude, latitude };
+  } catch (error) {
+    return `Failed to fetch location data: ${error}`;
+  }
+}
